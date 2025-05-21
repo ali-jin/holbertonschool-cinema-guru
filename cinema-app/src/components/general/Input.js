39d366259d3bcb963dd2af9e8143d0896c421654
ali-jin <alina.jin.contact@gmail.com>
2025-05-21 12:33:18 +0200

    implement react app

---
+++ cinema-app/src/components/general/Input.js
@@ -0,0 +1,27 @@
+import 'normalize.css';
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export default function Input({
+    label, type, className, value, setValue, icon=null, inputAttributtes={}
+}) {
+    const handleInput = (event) => {
+        setValue(event.target.value);
+    };
+    return(
+        <div className={className}>
+            <label htmlFor={label}>
+                {icon && <FontAwesomeIcon icon={icon} />}
+            </label>
+            <input 
+                type={type}
+                className={className}
+                value={value}
+                onChange={handleInput}
+                id={label}
+                {...inputAttributtes}
+            />
+        </div>
+    );
+}
